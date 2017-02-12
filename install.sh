81ffcb05c842105304e93286b2671467c41a734d
Sudokamikaze <pulshencode@outlook.com>
2017-02-13 01:48:18 +0200

    New feature, when pacman-mirrorlist being updated via pacman -Syu will use hook to call reflect_mirrors.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "x$(id -u)" != 'x0' ]; then
+    echo 'Error: this script can only be executed by root'
+    exit 1
+fi
+
+function install {
+if [ "$op" == "install" ]
+cp reflect_mirrors.sh /bin/reflect_mirrors.sh
+cp mirrors.hook /etc/pacman.d/hooks/mirrors.hook
+else
+rm /bin/reflect_mirrors.sh
+rm /etc/pacman.d/hooks/mirrors.hook
+fi
+}
+
+if ! [ -f /bin/reflect_mirrors.sh ]; then
+echo "Installing..."
+install
+op=install
+else
+echo "Removing..."
+install
+fi
