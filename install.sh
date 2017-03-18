1ba4032130afcd7d9a5ccca53b511f3edfab6118
Sudokamikaze <pulshencode@outlook.com>
2017-03-18 12:04:36 +0200

    Modify install script for PKGBUILD usage

--- install.sh
+++ install.sh
@@ -8,20 +8,49 @@ fi
 function install {
 if [ "$op" == "install" ]; then
 sed -i "s/hardcoded=false/hardcoded=true/g" ./reflect_mirrors.sh
-cp reflect_mirrors.sh /bin/reflect_mirrors.sh
-cp mirrors.hook /etc/pacman.d/hooks/mirrors.hook
+cp reflect_mirrors.sh $paths/reflect_mirrors.sh
+# cp mirrors.hook /etc/pacman.d/hooks/mirrors.hook
 sed -i "s/hardcoded=true/hardcoded=false/g" ./reflect_mirrors.sh
 else
-rm /bin/reflect_mirrors.sh
+rm $paths/reflect_mirrors.sh
 rm /etc/pacman.d/hooks/mirrors.hook
 fi
 }
 
-if ! [ -f /bin/reflect_mirrors.sh ]; then
-echo "Installing..."
-op=install
-install
-else
-echo "Removing..."
-install
+function operations {
+  if ! [ -f $paths/reflect_mirrors.sh ]; then
+  echo "Installing..."
+  op=install
+  install
+  else
+  echo "Removing..."
+  install
+  fi
+}
+
+function call {
+  case "$opt" in
+    i) paths=$OPTARG
+    ;;
+    *) paths="/bin/"
+    ;;
+  esac
+  operations
+}
+
+if [ $# = 0 ]; then
+  operations
 fi
+
+while getopts "i:" opt ;
+do
+  case $opt in
+    i)
+    call
+    exit 1
+    ;;
+    *) 
+    exit 1
+    ;;
+esac
+done
