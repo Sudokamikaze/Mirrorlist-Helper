76798b155d790f037e2e99ddf667071edf264e78
sudokamikaze <pulshencode@outlook.com>
2017-11-06 16:24:05 +0200

    Re-work scripts
    Signed-off-by: sudokamikaze <pulshencode@outlook.com>

--- reflect_mirrors.sh
+++ reflect_mirrors.sh
@@ -1,69 +1,29 @@
 #!/bin/bash
 
+function check_exist {
+  if [ -f "/etc/pacman.d/mirrorlist.pacnew" ]; then
+  sudo rm mirrorlist.pacnew
+  fi
+}
+
 function generate {
-cd /tmp
+check_exist
+cd /etc/pacman.d/
 echo "Downloading newly generated mirrorlist"
-wget -q -O mirrorlist.tmp https://www.archlinux.org/mirrorlist/?country=all&protocol=http&ip_version=4
-sudo cp mirrorlist.tmp /etc/pacman.d/mirrorlist.pacnew
-rm mirrorlist.tmp
-generated=true
-check
+sudo wget -q -O mirrorlist.pacnew https://www.archlinux.org/mirrorlist/?country=all&protocol=http&ip_version=4
+sudo reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist.pacnew
 }
 
-function reflect {
-  sudo reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist.pacnew
-  cd /etc/pacman.d/
-  if  [ "$generated" == "true" ]; then
-  sudo mv mirrorlist.pacnew mirrorlist
-else
-  sudo rm mirrorlist && sudo mv mirrorlist.pacnew mirrorlist
-fi
-}
-
-function check {
-varforcheck=$(ls /etc/pacman.d | grep "mirrorlist.pacnew")
-case "$varforcheck" in
-  mirrorlist.pacnew)
-  echo " "
-  echo "New mirrorlist was found."
-  echo " "
-  reflect
+case "$1" in
+  "--silent")
+  generate
   ;;
   *)
-  if [ "$opt" != "h" ]; then
-  echo " "
-  echo "New mirrorlist was not found."
-  echo " "
-  echo -n "Do you want to generate new mirrorlist?[Y/N]: "
-  read newmirror
-  if [ "$newmirror" == "y" ]; then
-  generate
-elif [ "$newmirror" == "Y" ]; then
-  generate
-fi
-else
-  echo "Mirrolist not found"
-  exit 1
-fi
-esac
-
-}
-
-if [ $# = 0 ]; then
-  echo -n "Do you want to processed mirrors?[Y/N]: "
-  read menu
-  case "$menu" in
-    y|Y) check
-    ;;
-    n|N|*) exit
-    ;;
+  echo -n "Do you want to download mirrors?: "
+  read choise
+  case "$choise" in
+    y|Y) generate ;;
+    *) exit 1 ;;
   esac
-fi
-
-while getopts "h" opt ;
-do
-  case $opt in
-    h) check
-    ;;
+  ;;
 esac
-done
